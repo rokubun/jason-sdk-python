7592bd64afd833e1a65c5e12454955f1be8fae59
Miquel Garcia <miquel.garcia@rokubun.cat>
2020-03-23 14:25:28 +0100

    doc: add description fields in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='jason-gnss',
     version_cc='{version}',
+    author='Rokubun',
+    author_email='info@rokubun.cat',
+    description='Python bindings for Jason Positioning-as-a-Service: Rokubun GNSS processing engine in the cloud.',
+    license='http://opensource.org/licenses/MIT',
+    url="https://github.com/rokubun/jason-gnss",
     setup_requires=['setuptools-git-version-cc'],
     packages=["jason_gnss"],
     include_package_data=True,
