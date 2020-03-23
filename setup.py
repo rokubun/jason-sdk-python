fc35a59378db99a48ce5819a5081d2b765032d75
Miquel Garcia <miquel.garcia@rokubun.cat>
2020-03-23 14:42:19 +0100

    doc: add long description from README.md file

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import find_packages, setup
 
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='jason-gnss',
     version_cc='{version}',
     author='Rokubun',
     author_email='info@rokubun.cat',
     description='Python bindings for Jason Positioning-as-a-Service: Rokubun GNSS processing engine in the cloud.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='http://opensource.org/licenses/MIT',
     url="https://github.com/rokubun/jason-gnss",
     setup_requires=['setuptools-git-version-cc'],
